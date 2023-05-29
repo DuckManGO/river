2a43f2fa56b2ada5b69a76bf1f014231dd18c76a
Kevin Cham <cheezillakc@gmail.com>
2023-05-29 18:16:36 +1200

    attention

--- river/drift/attention.py
+++ river/drift/attention.py
@@ -1,8 +1,29 @@
 from __future__ import annotations
 import numpy
 import scipy
+from tensorflow import matmul, math, cast, float32
+from tensorflow.python.keras.layers import Layer
+from keras.backend import softmax
 
 from river.base import DriftDetector
+
+class DotProductAttention(Layer):
+    def __init__(self, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+ 
+    def call(self, queries, keys, values, d_k, mask=None):
+        # Scoring the queries against the keys after transposing the latter, and scaling
+        scores = matmul(queries, keys, transpose_b=True) / math.sqrt(cast(d_k, float32))
+ 
+        # Apply mask to the attention scores
+        if mask is not None:
+            scores += -1e9 * mask
+ 
+        # Computing the weights by a softmax operation
+        weights = softmax(scores)
+ 
+        # Computing the attention by a weighted sum of the value vectors
+        return matmul(weights, values)
 
 class ATTENTION(DriftDetector):
 
@@ -10,8 +31,8 @@ class ATTENTION(DriftDetector):
         super().__init__()
         self._reset()
 
-        self.value = []
         self.value_size = 10
+        self.value = [0 for i in range(self.value_size)]
 
     def _reset(self):
         super()._reset()
@@ -21,16 +42,20 @@ class ATTENTION(DriftDetector):
         if self.drift_detected:
             self._reset()
 
-        self.value.append(x)
-        if len(self.value) > self.value_size:
-            self.value.pop(0)
-        """
-        attention_score = numpy.matmul(self.value, numpy.transpose(self.value, (1, 2)))
+        self.value.append([x])
+        self.value.pop(0)
+ 
+        d_k = 1  # Dimensionality of the linearly projected queries and keys
+        
+        queries = numpy.array([self.value])
+        keys = numpy.array([self.value])
+        values = numpy.array([self.value])
+        
+        print(values)
 
-        attention_prob = scipy.special.softmax(attention_score)
-
-        attention_values = numpy.matmul(attention_prob, self.value)
-
-        print(attention_values)
-        """
+        attention = DotProductAttention()
+        print(attention(queries, keys, values, d_k))
+        
         return self
+    
+    
